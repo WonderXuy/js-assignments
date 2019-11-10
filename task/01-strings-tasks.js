cd2d56e258e35de5a75d0bdf1cd6c501dc57b6d8
Veka Dzmitry <vekodmitriy0@gmail.com>
2019-11-10 18:53:03 +0300

    feat: implement extractEmails function

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -201,7 +201,7 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-    throw new Error('Not implemented');
+    return str.split(';');
 }
 
 /**
