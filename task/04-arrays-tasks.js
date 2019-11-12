7580611838d1837fe4f7601f3211f38851899af0
Veka Dzmitry <vekodmitriy0@gmail.com>
2019-11-12 12:21:49 +0300

    04-arrays-tasks/feat: implement selectMany function

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -600,7 +600,11 @@ function group(array, keySelector, valueSelector) {
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-    throw new Error('Not implemented');
+   let result = [];
+   arr.map(function(item) {
+      result = result.concat(childrenSelector(item));
+   });
+   return result;
 }
 
 
