0e235a1967eedcd548ee02da02031c32bac2a8c9
Veka Dzmitry <vekodmitriy0@gmail.com>
2019-11-12 08:04:31 +0300

    04-arrays-tasks/feat: implement toArrayOfSquares function

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -223,7 +223,7 @@ function toCsvText(arr) {
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
 function toArrayOfSquares(arr) {
-   throw new Error('Not implemented');
+   return arr.map(item => Math.pow(item, 2));
 }
 
 
