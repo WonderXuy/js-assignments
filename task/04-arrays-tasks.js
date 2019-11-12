87adde1e2419790c6203c208ca280b0be4f24e2f
Veka Dzmitry <vekodmitriy0@gmail.com>
2019-11-12 07:34:28 +0300

    04-arrays-tasks/feat: implement removeFalsyValues function

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -102,7 +102,9 @@ function getArrayOfStrings(arr) {
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
 function removeFalsyValues(arr) {
-   throw new Error('Not implemented');
+   return arr.filter(function(item) {
+      if(Boolean(item) == true) return item;
+   });
 }
 
 /**
